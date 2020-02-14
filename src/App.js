bcc1c92c659ed7b02294fb2ef3b3e1345e08bd16
Marcelo Anselmo <pilatus@iMac.local>
2020-02-14 17:12:16 -0300

    my editing files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from 'react';
+import './App.css';
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Products from './pages/ProductsPage';
+import Contact from './pages/ContactPage';
+import SingleProduct from './pages/SingleProductPage';
+import Cart from './pages/CartPage.js';
+import Default from './pages/Default.js';
 
-import Home from "./pages/HomePage";
-import About from "./pages/AboutPage";
-import Products from "./pages/ProductsPage";
-import Contact from "./pages/ContactPage";
-import SingleProduct from "./pages/SingleProductPage";
-import Cart from "./pages/CartPage.js";
-import Default from "./pages/Default.js";
+import { Route, Switch } from 'react-router-dom';
 
-import { Route, Switch } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import SideCart from "./components/SideCart";
-import Footer from "./components/Footer";
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import SideCart from './components/SideCart';
+import Footer from './components/Footer';
 class App extends Component {
   render() {
     return (
@@ -25,12 +23,12 @@ class App extends Component {
         <Sidebar />
         <SideCart />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/products" exact component={Products} />
-          <Route path="/products/:id" component={SingleProduct} />
-          <Route path="/cart" component={Cart} />
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/products/:id' component={SingleProduct} />
+          <Route path='/cart' component={Cart} />
           <Route component={Default} />
         </Switch>
         <Footer />
